411dc23ade05d0711f4c9aefe45c9e832015a747
Brajender Kaur <62285239+BrijKaur@users.noreply.github.com>
2020-04-10 16:45:17 +0530

    Deploy.bat updated

--- Deploy.bat
+++ Deploy.bat
@@ -1,2 +1,21 @@
 
-echo "Deploying Build : %date% : %time%"
+echo "Deploying Build"
+
+F:
+cd F:\RetailBanking\retailbank-app\
+
+echo "copying the war file to be deploy at the location were dockerfile is present"
+copy target\retailbank-app-1.0.war ..\docker\
+
+cd ..\docker\
+
+echo "Building the docker Image from the dockerfile and tagging it as retailbankimage"
+docker build -t retailbankimage .
+
+echo "Removing the previous retailbankcontainer container if present"
+docker rm -f retailbankcontainer
+
+echo "Using the docker image the conatiner is run -- this will deply the application on the WildFly(JBoss) Server"
+docker run -d -p 8081:8081 --name retailbankcontainer retailbankimage
+
+echo "Application deployed successfully at http://<Deployment_Server_IP>:8081/ "
